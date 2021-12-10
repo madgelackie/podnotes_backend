1379dcc950fd3089e76afb473db57fd2848d5052
madgelackie <marionmlackie@gmail.com>
2021-12-10 23:10:58 +0000

    initial files

---
+++ src/main/java/com/example/podnotesservice/podnotesBackend/PodnotesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.podnotesservice.podnotesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodnotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodnotesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/podnotesservice/podnotesBackend/PodnotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.podnotesservice.podnotesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodnotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
