fdd52c9e6313304db44f3c3da58aa0bc90195357
madgelackie <marionmlackie@gmail.com>
2021-12-11 15:33:02 +0000

    further crud actions working for channel, checked with insomnia

--- src/main/java/com/example/podnotesservice/podnotesBackend/controller/ChannelController.java
+++ src/main/java/com/example/podnotesservice/podnotesBackend/controller/ChannelController.java
@@ -5,8 +5,7 @@ import com.example.podnotesservice.podnotesBackend.repository.ChannelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +18,17 @@ public class ChannelController {
     @GetMapping(value = "/channels")
     public ResponseEntity<List<Channel>> getAllChannels(){
         return new ResponseEntity<>(channelRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/channels/{id}")
+    public ResponseEntity getChannel(@PathVariable Long id){
+        return new ResponseEntity<>(channelRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "channels")
+    public ResponseEntity createChannel(@RequestBody Channel channel){
+        channelRepository.save(channel);
+        return new ResponseEntity<>(channel, HttpStatus.CREATED);
     }
 
 }
