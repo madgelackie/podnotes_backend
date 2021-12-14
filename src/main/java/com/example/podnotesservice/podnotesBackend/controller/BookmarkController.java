2f1c31d588318aa01c0d52b18ffa1269055ee5b5
madgelackie <marionmlackie@gmail.com>
2021-12-14 21:24:10 +0000

    bookmarkRepository updated

--- src/main/java/com/example/podnotesservice/podnotesBackend/controller/BookmarkController.java
+++ src/main/java/com/example/podnotesservice/podnotesBackend/controller/BookmarkController.java
@@ -1,12 +1,12 @@
 package com.example.podnotesservice.podnotesBackend.controller;
 
 import com.example.podnotesservice.podnotesBackend.models.Bookmark;
+import com.example.podnotesservice.podnotesBackend.models.Channel;
 import com.example.podnotesservice.podnotesBackend.repository.BookmarkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +17,15 @@ public class BookmarkController {
     BookmarkRepository bookmarkRepository;
 
     @GetMapping(value = "/bookmarks")
+    @CrossOrigin(origins = "*", maxAge = 3600)
     public ResponseEntity<List<Bookmark>> getAllBookmarks(){
         return new ResponseEntity<>(bookmarkRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/bookmarks")
+    @CrossOrigin(origins = "*", maxAge = 3600)
+    public ResponseEntity createBookmark(@RequestBody Bookmark bookmark){
+        bookmarkRepository.save(bookmark);
+        return new ResponseEntity (bookmark, HttpStatus.CREATED);
     }
 }
